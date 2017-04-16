4ac22bac6aace27373331e6a455777e85c27996e
Tony Tran <toant713@gmail.com>
2017-04-16 14:35:28 -0400

    Added policy and roles

--- aws/src/terraform/main.tf
+++ aws/src/terraform/main.tf
@@ -12,6 +12,17 @@ variable "region" {
   default = "us-east-1"
 }
 
+resource "aws_iam_role" "parse_Lambda" {
+  name = "parse_Lambda"
+  assume_role_policy = "${file("../policies/lambda-role.json")}"
+}
+
+resource "aws_iam_role_policy" "parse_lambda_policy" {
+  name = "parse_lambda_policy"
+  role = "${aws_iam_role.parse_Lambda.id}"
+  policy = "${file("../policies/lambda-policy.json")}"
+}
+
 resource "aws_elasticache_cluster" "FrequencyRedis" {
   cluster_id = "frequency-cluster"
   engine = "redis"
@@ -21,3 +32,6 @@ resource "aws_elasticache_cluster" "FrequencyRedis" {
   num_cache_nodes = 1
   parameter_group_name = "default.redis3.2"
 }
+
+
+
