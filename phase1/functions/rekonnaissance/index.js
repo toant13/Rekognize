6d3cede432f2bf541169bebd3b6c0f295656cd56
Tony Tran <toant713@gmail.com>
2017-04-23 14:46:30 -0400

    Added environment variable for local testing

--- phase1/functions/rekonnaissance/index.js
+++ phase1/functions/rekonnaissance/index.js
@@ -1,8 +1,21 @@
 const redis = require('redis');
 
+function getRedisClient() {
+  const lambdaEnv = (process.env.LAMBDA_ENV || 'AWS');
+  if (lambdaEnv === 'LOCAL') {
+    return redis.createClient({ host: 'localhost', port: 6379 });
+  }
+
+  return redis.createClient({
+    host: 'frequency-cluster.u2yttb.0001.use1.cache.amazonaws.com',
+    port: 6379,
+  });
+}
+
 exports.handler = function (e, ctx, cb) {
-  // const redisClient = redis.createClient({ host: 'frequency-cluster.u2yttb.0001.use1.cache.amazonaws.com', port: 6379 });
-  const redisClient = redis.createClient({host: 'localhost', port: 6379});
+  console.log('changed backed to required', redis);
+
+  const redisClient = getRedisClient();
 
   redisClient.on('ready', () => {
     console.log('Redis is ready');
@@ -13,5 +26,6 @@ exports.handler = function (e, ctx, cb) {
   });
 
   console.log('processing event: %s', redisClient);
-  cb(null, { hello: 'world' });
+  cb(null, {hello: 'world'});
 };
+
